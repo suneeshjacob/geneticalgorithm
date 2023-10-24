b745fa5b0227c910b00ee8e0b16728c5a4ba2b25
Suneesh Jacob <suneeshjacob@gmail.com>
2023-10-24 15:07:56 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,6 +1,5 @@
 def binary_range(a, b, chromosome_length = 10):
     n = 2**chromosome_length - 1
     h = (b-a)/n
-    dictionary = dict([(bin(i+1)[2:].zfill(chromosome_length),i*h) for i in range(n)])
+    dictionary = dict([(bin(i)[2:].zfill(chromosome_length),a+i*h) for i in range(n+1)])
     return dictionary
-
